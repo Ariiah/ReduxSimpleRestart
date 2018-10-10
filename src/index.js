e1ee18ddec23850f0eb913735e3d086f33db97c4
Kelly L <lock2846@gmail.com>
2018-10-10 14:06:43 -0600

    state props added to VideoList and added VideoDetail with video prop

--- src/index.js
+++ src/index.js
@@ -1,23 +1,35 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search'
 import SearchBar from './components/search_bar.js'
-
+import VideoList from './components/video_list.js'
+import VideoDetail from './components/video_detail.js'
 const API_KEY = process.env.REACT_APP_API_KEY;
-console.log(`${process.env.REACT_APP_API_KEY}`);
 
 
 
-YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
-  console.log(data);
-})
+class App extends Component {
+  constructor(props) {
+    super(props)
 
-const App = () => {
-  return (
-    <div>
-    <SearchBar />
-  </div>
-  )
+    this.state = {
+       videos: []
+     }
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({ videos })
+      // means the same as this.setState({videos: videos})
+    })
+  }
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoDetail video={this.state.videos[0]} />
+        <VideoList videos={this.state.videos} />
+      </div>
+    )
+  }
 }
 
-ReactDOM.render(<App />, document.querySelector('.container'))
+ReactDOM.render(<App/>, document.querySelector('.container'))
