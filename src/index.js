2b736ed2eb91cf843e2364cc28efaed6b2302d9f
Kelly L <lock2846@gmail.com>
2018-10-09 22:38:22 -0600

    removed extra code

--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import YTSearch from 'youtube-api-search'
+import SearchBar from './components/search_bar.js'
 
-import App from './components/app';
-import reducers from './reducers';
+const API_KEY = process.env.REACT_APP_API_KEY;
+console.log(`${process.env.REACT_APP_API_KEY}`);
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+
+YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
+  console.log(data);
+})
+
+const App = () => {
+  return (
+    <div>
+    <SearchBar />
+  </div>
+  )
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'))
